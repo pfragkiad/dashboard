a3676618ede573834a63a3b38d0f83d2e59568ec
Pavlos Fragkiadoulakis <57967801+pfragkiad@users.noreply.github.com>
2023-06-01 03:02:18 +0300

    Add index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React  from "react";
 import  ReactDOM  from "react-dom";
 
+//https://github.com/adrianhajdin/project_syncfusion_dashboard
+//https://www.youtube.com/watch?v=jx5hdo50a2M
+
+
+//npx create-react-app ./
+//set package.json (copy from github e.g.)
+//npm install --legacy-peer-deps
+
+
+//npm start
 
 import App from '/App';
 
